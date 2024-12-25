e99a6d6b6ae68e3967f624bdd4e439c5a099cee3
ozerbaykal <baykalozer87@gmail.com>
2024-12-25 07:24:43 +0300

    doc: add src and create project structure

---
+++ src/theme/colors.js
@@ -0,0 +1,13 @@
+const ThemeColors = {
+    yellow: "#F6BB54",
+    blue: "#9D9BFF",
+    pink: "#EDA7FF",
+    lightPink: "#FFAFAF",
+    green: "#5DD669",
+    white: "#FFFFFF",
+    black: "#181818",
+    gray: "#DDDDDD",
+
+}
+
+export { ThemeColors };
